a3033cd2567c032865f28d9d3258157659550e9c
kresooo <kresimir.pehar@pubnub.com>
2019-08-22 12:58:58 +0200

    add test

--- src/context/PubNubConsumer.test.tsx
+++ src/context/PubNubConsumer.test.tsx
@@ -1,17 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { PubNubConsumer } from './PubNubConsumer';
+const value = 4;
 
-describe('render component', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PubNubConsumer />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+test('number', () => {
+  expect(value).toBe(4);
 });
-
-// const value = 4;
-
-// describe('value', () => {
-//   expect(value).tob
-// })
